9d1ee8b264f6035948043b25c3d76bce2f0ff515
YuliaKokorieva <Yuliya.Pastushenko@gmail.com>
2022-08-12 14:11:55 +0300

    ex 9.15 final

--- ex_9.14_react/src/types.tsx
+++ ex_9.14_react/src/types.tsx
@@ -1,28 +1,28 @@
-export interface CoursePartBase {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
   type: string;
 }
 
-export interface CoursePartBaseExtended extends CoursePartBase {
+interface CoursePartBaseExtended extends CoursePartBase {
   description: string;
 }
 
-export interface CourseNormalPart extends CoursePartBaseExtended {
+interface CourseNormalPart extends CoursePartBaseExtended {
   type: "normal";
 }
 
-export interface CourseProjectPart extends CoursePartBase {
+interface CourseProjectPart extends CoursePartBase {
   type: "groupProject";
   groupProjectCount: number;
 }
 
-export interface CourseSubmissionPart extends CoursePartBaseExtended {
+interface CourseSubmissionPart extends CoursePartBaseExtended {
   type: "submission";
   exerciseSubmissionLink: string;
 }
 
-export interface CourseSpecialPart extends CoursePartBaseExtended {
+interface CourseSpecialPart extends CoursePartBaseExtended {
   type: "special";
   requirements: Array<string>;
 }
